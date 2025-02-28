4c560923481849ce64574ccc176ab3a2c0d9fe6d
Draickins <draickins@gmail.com>
2025-02-28 16:24:05 +0100

    added function to sort dictionary and print report with alpha. characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_counter, character_counter
+from stats import word_counter, character_counter, sorted_list
 
 
 def get_book_text(file_path):
@@ -7,10 +7,27 @@ def get_book_text(file_path):
 
 
 def main():
+    path = "books/frankenstein.txt"
     frankenstein_book = get_book_text("/Users/stepankucera/workspace/github/draickins/bookbot/books/frankenstein.txt")
     number_of_words = word_counter(frankenstein_book)
+    character_counter_dict = character_counter(frankenstein_book)
 
-    print(F"{number_of_words} words found in the document")
-    print(character_counter(frankenstein_book))
+    sorted_chars = sorted_list(character_counter_dict)
+
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+
+
+    for char_dict in sorted_chars:
+        for char, count in char_dict.items():
+            if char.isalpha():
+                print(f"{char}: {count}")
+
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+from test.test_configparser import SortedDict
 def word_counter(book):
     words = book.split()
     counter = 0
@@ -10,3 +11,13 @@ def character_counter(book):
     for char in book:
         character_dict[char.lower()] = character_dict.get(char.lower(), 0) + 1
     return character_dict
+
+def sorted_list(char_dict):
+    def sort_on(dict_item):
+        return list(dict_item.values())[0]
+
+    dict_list = []
+    for char in char_dict:
+        dict_list.append({char: char_dict[char]})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
